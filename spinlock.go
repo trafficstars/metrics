4e6bd656683c69582ff1a784ba3b5502173625a2
Dmitry Yu Okunev <xaionaro@wisebits.com>
2019-02-16 13:30:19 +0000

    Defensive fix for Spinlock

--- spinlock.go
+++ spinlock.go
@@ -9,12 +9,13 @@ import (
 type Spinlock int32
 
 func (s *Spinlock) Lock() {
-	for atomic.AddInt32((*int32)(s), 1) != 1 {
-		atomic.AddInt32((*int32)(s), -1)
+	for !atomic.CompareAndSwapInt32((*int32)(s), 0, 1) {
 		time.Sleep(time.Nanosecond * 300 * time.Duration(rand.Intn(30)))
 	}
 }
 
 func (s *Spinlock) Unlock() {
-	atomic.AddInt32((*int32)(s), -1)
+	for !atomic.CompareAndSwapInt32((*int32)(s), 1, 0) {
+		time.Sleep(time.Nanosecond * 300 * time.Duration(rand.Intn(30)))
+	}
 }
