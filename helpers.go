e7317adfa66083aec8b6ad226c611607fe842ed8
vintorez <vintorez@syss-MacBook-Pro-2.local>
2019-03-16 16:06:40 +0200

    develop: reformat TagsToMap function

--- helpers.go
+++ helpers.go
@@ -1,9 +1,12 @@
 package metrics
 
-import "github.com/sirupsen/logrus"
-
-func AnyTagsToLogrusFields(tags AnyTags) logrus.Fields {
-	fields := logrus.Fields{}
+func TagsToMap(tags AnyTags, fieldMaps... map[string]interface{}) map[string]interface{} {
+	fields := map[string]interface{}{}
+	for _, fieldMap := range fieldMaps {
+		for k, v := range fieldMap {
+			fields[k] = v
+		}
+	}
 	tags.Each(func(k string, v interface{}) bool {
 		fields[k] = v
 		return true
